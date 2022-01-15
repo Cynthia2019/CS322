259ef32d3fc29e4b263aaeab26ded7288daa748c
Cynthia2019 <ccynthia2019@gmail.com>
2022-01-15 16:36:48 -0600

    code generator start

--- L1/src/code_generator.cpp
+++ L1/src/code_generator.cpp
@@ -14,12 +14,47 @@ namespace L1{
      */ 
     std::ofstream outputFile;
     outputFile.open("prog.S");
-   
     /* 
      * Generate target code
      */ 
     //TODO
+    //write pushq for registers
+    outputFile << ".text\n"; 
+    outputFile << " .globl go\n"; 
+    outputFile << ".go: \n"; 
+    outputFile << " pushq %rbx\n";
+    outputFile << " pushq %rbp\n";
+    outputFile << " pushq %r12\n";
+    outputFile << " pushq %r13\n";
+    outputFile << " pushq %r14\n";
+    outputFile << " pushq %r15\n";
+    std::string name = '_' + p.entryPointLabel; 
+    cout << "programe name: " << name << endl; 
+    outputFile << " call " + name + "\n"; 
+    outputFile << " popq %r15\n";
+    outputFile << " popq %r14\n";
+    outputFile << " popq %r13\n";
+    outputFile << " popq %r12\n";
+    outputFile << " popq %rbp\n";
+    outputFile << " popq %rbx\n";
+    outputFile << " retq\n"; 
 
+    for(auto f : p.functions) {
+      outputFile << '_' + f->name + ':\n'; 
+      for(Instruction* i : f->instructions){
+        //if assignment
+        Instruction_assignment *a = dynamic_cast<Instruction_assignment *>(i);
+        std::string translated = "\t\t"; 
+        if(a != nullptr) {
+          translated.append("movq "); 
+          if(a->isARegister){
+            translated.append('%' + a->r + ',');
+             
+          }
+        }
+      }
+    }
+    
     /* 
      * Close the output file.
      */ 
