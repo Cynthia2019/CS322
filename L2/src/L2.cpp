ba13624b108bd2603978a34fbb0b46594297fe9b
Cynthia2019 <ccynthia2019@gmail.com>
2022-01-23 19:52:41 -0600

    bug fix

--- L2/src/L2.cpp
+++ L2/src/L2.cpp
@@ -65,6 +65,9 @@ namespace L2 {
     if (src->get_type() == item_variable || src->get_type() == item_register) {
       v.push_back((src));
     }
+    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
+      v.push_back((dst));
+    }
     return v;
   }
 
@@ -82,6 +85,9 @@ namespace L2 {
     if (src->get_type() == item_variable || src->get_type() == item_register) {
       v.push_back((src));
     }
+    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
+      v.push_back((dst));
+    }
     return v;
   }
 
@@ -97,6 +103,9 @@ namespace L2 {
     vector<Item *> v;
     if (src->get_type() == item_variable || src->get_type() == item_register) {
       v.push_back((src));
+    }
+    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
+      v.push_back((dst));
     }
     return v;
   }
@@ -118,14 +127,17 @@ namespace L2 {
   }
 
   vector<Item *> Instruction_stack::get_kill_set() {
-    vector<Item *> v;
-    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
-      v.push_back((dst));
-    }
-    return v;
+    return {}; 
   }
   vector<Item *> Instruction_compare::get_gen_set() { 
-    return {}; 
+    vector<Item *> v;
+    if (oprand1->get_type() == item_variable || oprand1->get_type() == item_register) {
+      v.push_back((oprand1));
+    }
+    if (oprand2->get_type() == item_variable || oprand2->get_type() == item_register) {
+      v.push_back((oprand2));
+    }
+    return v;
   }
 
   vector<Item *> Instruction_compare::get_kill_set() {
@@ -137,7 +149,14 @@ namespace L2 {
   }
 
   vector<Item *> Instruction_cjump::get_gen_set() { 
-    return {};
+    vector<Item *> v;
+    if (oprand1->get_type() == item_variable || oprand1->get_type() == item_register) {
+      v.push_back((oprand1));
+    }
+    if (oprand2->get_type() == item_variable || oprand2->get_type() == item_register) {
+      v.push_back((oprand2));
+    }
+    return v;
   }
 
   vector<Item *> Instruction_cjump::get_kill_set() {
@@ -240,19 +259,41 @@ namespace L2 {
   }
 
   vector<Item *> Instruction_at::get_gen_set() { 
-    return {};
+    vector<Item *> v;
+    if (src_mult->get_type() == item_variable || src_mult->get_type() == item_register) {
+      v.push_back(src_mult);
+    }
+    if (src_add->get_type() == item_variable || src_add->get_type() == item_register) {
+      v.push_back(src_add);
+    }
+    return v;
   }
 
   vector<Item *> Instruction_at::get_kill_set() {
-    return {};
+    vector<Item *> v;
+    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
+      v.push_back(dst);
+    }
+    return v;
   }
 
   vector<Item *> Instruction_load_aop::get_gen_set() { 
-    return {};
+    vector<Item *> v;
+    if (src->get_type() == item_variable || src->get_type() == item_register) {
+      v.push_back(src);
+    }
+    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
+      v.push_back(dst);
+    }
+    return v;
   }
 
   vector<Item *> Instruction_load_aop::get_kill_set() {
-    return {};
+    vector<Item *> v;
+    if (dst->get_type() == item_variable || dst->get_type() == item_register) {
+      v.push_back(src);
+    }
+    return v;
   }
   vector<Item *> Instruction_goto::get_gen_set() { 
     return {};
