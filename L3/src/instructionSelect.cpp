51624884c11342a562ce5d610168fa47c16a5dd2
Cynthia2019 <ccynthia2019@gmail.com>
2022-02-10 23:20:39 -0600

    tree

--- L3/src/instructionSelect.cpp
+++ L3/src/instructionSelect.cpp
@@ -7,6 +7,7 @@ namespace L3 {
     void Tree::visit(Instruction_ret* i) {
         this->i = i; 
         TreeNode* node = new TreeNode();
-        node->op = i->
+        node->op = i->op; 
+        
     }
 }
