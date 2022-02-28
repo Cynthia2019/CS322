2fbf0a860dfb9f76ec41ba44cb034f79a58f839b
Cynthia2019 <ccynthia2019@gmail.com>
2022-02-27 23:55:07 -0600

    encode

--- LA/src/coding.h
+++ LA/src/coding.h
@@ -6,4 +6,26 @@ using namespace std;
 namespace LA {
     vector<Item*> toDecode(Instruction* i); 
     vector<Item*> toEncode(Instruction* i); 
+    class Encoder : public Visitor {
+    public: 
+      Encoder(Function* f); 
+      void visit(Instruction_ret_not *i) override;
+      void visit(Instruction_ret_t *i) override;
+      void visit(Instruction_assignment *i) override;
+      void visit(Instruction_load *i) override;
+      void visit(Instruction_op *i) override;
+      void visit(Instruction_store *i) override;
+      void visit(Instruction_declare *i) override;
+      void visit(Instruction_br_label *i) override;
+      void visit(Instruction_br_t *i) override;
+      void visit(Instruction_call_noassign *i) override;
+      void visit(Instruction_call_assignment *i) override;
+      void visit(Instruction_label *i) override;
+      void visit(Instruction_length *i) override;
+      void visit(Instruction_array *i) override;
+      void visit(Instruction_tuple *i) override;
+    private: 
+      Function* f;
+  };
+    void encodeAll(Program &p, Function* f); 
 }
